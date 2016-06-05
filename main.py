01b19ea9632e122d6610621d65eea4337d2abb02
Matteo Rosati <mrosati@h-art.com>
2016-06-05 12:20:03 +0200

    add database support for locations

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from urllib.parse import urlparse
 import flask
 from dotenv import load_dotenv
 from flask import Flask, request
+from sqlalchemy import Table, Column, Sequence, Integer, String, MetaData
+from sqlalchemy import create_engine
 from werkzeug.contrib.cache import RedisCache
 
 from cinema_parser import CinemaParser
@@ -25,6 +27,22 @@ redis_cache = RedisCache(port=redis_url.port,
                          host=redis_url.hostname,
                          password=redis_url.password)
 
+##########################
+# create database engine #
+##########################
+engine = create_engine(os.environ.get('DATABASE_URL'))
+
+metadata = MetaData()
+
+# table definition
+locations_table = Table('locations', metadata,
+                        Column('id', Integer, Sequence('location_id_seq'), primary_key=True),
+                        Column('location', String, nullable=False))
+
+# create locations table (it checks table existence, so it's
+# safe to call multiple times)
+metadata.create_all(engine)
+
 
 @app.route('/')
 def api():
@@ -32,10 +50,25 @@ def api():
     cinema_name = request.args.get('cinema_name')
     movie_name = request.args.get('movie_name')
 
+    insert = locations_table.insert().values(location=str(near))
+
+    engine.connect()
+    engine.execute(insert)
+
     # Redis cache is injected as service in constructor
     parser = CinemaParser(near, app=app, cache=redis_cache)
 
     return flask.jsonify(parser.get(cinema_name=cinema_name, movie_name=movie_name))
+
+
+@app.route('/locations')
+def locations():
+    grouped_locations = engine.execute(
+        """
+        SELECT location, count(*) FROM locations GROUP BY location
+        """).fetchall()
+
+    return flask.jsonify(grouped_locations)
 
 
 if __name__ == '__main__':
